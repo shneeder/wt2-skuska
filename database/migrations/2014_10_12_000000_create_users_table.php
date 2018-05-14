4be7672e7491906750806b9ab1bb7411f78e211a
Lukas Snider <lukassnider@gmail.com>
2018-05-14 22:02:19 +0200

    Updated users table for migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,18 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('street');
+            $table->string('postal_code');
+            $table->string('town');
+            $table->float('latitude', 11, 2);
+            $table->float('longtitude', 11, 2);
+            $table->string('school_name');
+            $table->string('school_address');
+            $table->float('school_lat', 11, 2);
+            $table->float('school_lng', 11, 2);
+            $table->boolean('isAdmin')->default(false);
         });
     }
 
