3f23abf12357b569880ae4522596d96d1e143192
KamilBT <KamilBT@github.com>
2018-05-14 19:42:48 +0200

    Bod11
    priadanie kodu pre bod11, no neviem ako to potom este cele dat dokopy ...

---
+++ public/bod11/config.php
@@ -0,0 +1,19 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "WT_mapa";
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", $dbname);
+
+$dbconfig = array(
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'dbname' => 'WT_mapa',
+);
+
+?>
