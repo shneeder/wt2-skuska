9c82c354ebfce8afe13a23558bc56b7e3f37a6ba
KamilBT <KamilBT@github.com>
2018-05-18 23:49:56 +0200

    uprava

--- public/bod11/clickHandler.js
+++ public/bod11/clickHandler.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     $("#startGeocode").click(function(){
       $.ajax({
          success: function(){
-            window.location.href = '';  
+            window.location.href = 'http://147.175.98.234/wt2-skuska/public/bod11/geocoding.php';  
             }     
       }); 
     });
     $("#naspat").click(function(){
       $.ajax({
          success: function(){
-            window.location.href = 'http://localhost/nove/admin.php';  
+            window.location.href = 'http://147.175.98.234/wt2-skuska/public/bod11/admin.php';  
             }     
       }); 
     });
